0fddcb8ae69bedcbc00ef2facc6ed7635b8ce590
Wesley S dos Santos <113936684+WesleyS08@users.noreply.github.com>
2025-03-17 20:11:03 -0300

    feat(app): configura navegação e ajustes de interface
    - Implementa NavigationContainer como base da navegação
    - Oculta barra de status e barra de navegação
    - Adiciona suporte para gesture handler
    - Configura stack de autenticação como rota inicial

--- SALV_App/App.tsx
+++ SALV_App/App.tsx
@@ -1,11 +1,20 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+
 import { NavigationContainer } from '@react-navigation/native';
 import AuthStack from './src/navigation/AuthStack';
-import LoginEmail from './src/screens/LoginEmail';
+import { StatusBar } from 'expo-status-bar';
+import * as ScreenOrientation from 'expo-screen-orientation';
+import React, { useEffect } from 'react';
+import * as NavigationBar from 'expo-navigation-bar';
+
 export default function App() {
+  useEffect(() => {
+    NavigationBar.setVisibilityAsync('hidden');
+  }, []);
+
   return (
     <NavigationContainer>
+      <StatusBar hidden={true} />
       <AuthStack />
     </NavigationContainer>
   );
