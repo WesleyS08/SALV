7b8eae15be8cfe03ba8571d0f804694d093a5b4e
Wesley <113936684+WesleyS08@users.noreply.github.com>
2025-04-03 14:38:31 +0000

    definição de icones do app

--- SALV_App/App.tsx
+++ SALV_App/App.tsx
@@ -3,7 +3,6 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import AuthStack from './src/navigation/AuthStack';
 import { StatusBar } from 'expo-status-bar';
-import * as ScreenOrientation from 'expo-screen-orientation';
 import React, { useEffect } from 'react';
 import * as NavigationBar from 'expo-navigation-bar';
 import { DarkModeProvider } from './src/Global/DarkModeContext';
